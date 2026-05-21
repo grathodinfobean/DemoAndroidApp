7dc792789cd4204a59ada6dfe0e036ed5ea95025
Gourav Rathod <gourav.rathod@infobeans.com>
2026-05-21 12:43:22 +0530

    Added Util file with Test case

---
+++ app/src/main/java/com/example/demoandroidapp/util.kt
@@ -0,0 +1,6 @@
+package com.example.demoandroidapp
+
+class util {
+
+    fun sum(a:Int,b:Int)=a+b
+}
---
+++ app/src/test/java/com/example/demoandroidapp/MathUtilsTest.kt
@@ -0,0 +1,12 @@
+package com.example.demoandroidapp
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class MathUtilsTest {
+
+    @Test
+    fun checkSum() {
+        assertEquals(4, MathUtils.sum(2,3))
+    }
+}
